67a81ae9eec12d85ebbeda894e2bc91962d6b4e0
zbenkhelifa <zahire.benkhelfia@ac-versailles.fr>
2026-06-13 14:07:55 +0200

    debug: log key lengths to diagnose 403

--- post-tweet.js
+++ post-tweet.js
@@ -2,11 +2,22 @@ const { TwitterApi } = require('twitter-api-v2');
 const tweets = require('./tweets.json');
 
 async function postTweet() {
-  const client = new TwitterApi({
+  const keys = {
     appKey: process.env.TWITTER_API_KEY,
     appSecret: process.env.TWITTER_API_SECRET,
     accessToken: process.env.TWITTER_ACCESS_TOKEN,
     accessSecret: process.env.TWITTER_ACCESS_SECRET,
+  };
+  console.log('Vérification des clés :');
+  for (const [k, v] of Object.entries(keys)) {
+    console.log(`  ${k}: ${v ? v.slice(0, 6) + '...' + ' (longueur: ' + v.length + ')' : 'MANQUANT'}`);
+  }
+
+  const client = new TwitterApi({
+    appKey: keys.appKey,
+    appSecret: keys.appSecret,
+    accessToken: keys.accessToken,
+    accessSecret: keys.accessSecret,
   });
 
   // Sélection déterministe basée sur le jour de l'année (pas de répétition dans l'année)
